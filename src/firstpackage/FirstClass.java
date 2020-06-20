ad7d73e731c3204197a5428c62be66069c81fa68
ersinbilici <67049444+ersinbilici@users.noreply.github.com>
2020-06-20 21:30:49 +0200

    Update FirstClass.java
    conflict yaptik galiba

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -2,8 +2,7 @@ package firstpackage;
 
 public class FirstClass {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
 		System.out.println("conflict yapmak istiyoruz");
